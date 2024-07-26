8c91b1a8b41c375825a466edb42084e69a9f4c3f
Luciana <lt.shimoyama@gmail.com>
2024-07-26 09:26:44 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/luciana/curso_mongodb/CursoMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.luciana.curso_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciana/curso_mongodb/CursoMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.curso_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
