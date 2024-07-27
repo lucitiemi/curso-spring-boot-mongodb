05e5bbe9b1225e93d60b2ec6ef3e95509408ebe3
Luciana <lt.shimoyama@gmail.com>
2024-07-27 16:45:14 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luciana/curso_mongodb/services/PostService.java
@@ -0,0 +1,30 @@
+package com.luciana.curso_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luciana.curso_mongodb.domain.Post;
+import com.luciana.curso_mongodb.repositories.PostRepository;
+import com.luciana.curso_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	// busca User por id
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+
+	
+
+
+}
