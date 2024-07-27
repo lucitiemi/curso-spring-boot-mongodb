c761dda02c12d738004c3de9baca335d93c91b4d
Luciana <lt.shimoyama@gmail.com>
2024-07-27 15:12:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/luciana/curso_mongodb/services/UserService.java
+++ src/main/java/com/luciana/curso_mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luciana.curso_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luciana.curso_mongodb.domain.User;
 import com.luciana.curso_mongodb.repositories.UserRepository;
+import com.luciana.curso_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -21,5 +23,9 @@ public class UserService {
 	}
 	
 	
-
+	// buscar User por id
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
