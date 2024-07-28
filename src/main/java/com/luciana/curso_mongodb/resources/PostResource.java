d61c8984b9c9a3760152e6421c5581fdab2ec60b
Luciana <lt.shimoyama@gmail.com>
2024-07-28 00:49:36 -0300

    Consulta simples com query methods

--- src/main/java/com/luciana/curso_mongodb/resources/PostResource.java
+++ src/main/java/com/luciana/curso_mongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.luciana.curso_mongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luciana.curso_mongodb.domain.Post;
+import com.luciana.curso_mongodb.resources.util.URL;
 import com.luciana.curso_mongodb.services.PostService;
 
 @RestController
@@ -18,12 +22,20 @@ public class PostResource {
 	private PostService service;	
 	
 	
-	
 	// busca Post por id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	// busca Posts pelo titulo					// url: /posts/titlesearch?text=bom%20dia
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 
--- src/main/java/com/luciana/curso_mongodb/services/PostService.java
+++ src/main/java/com/luciana/curso_mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.luciana.curso_mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	
+	// busca Posts pelo titulo
+	public List<Post> findByTitle(String title) {
+		return repository.findByTitleContainingIgnoreCase(title);
+	}
 	
 
 	
